33bd080ec8eb6180d1eb28c92186d4ca5223e002
Fabiano Leite <fabiano.marechal@gmail.com>
2024-04-12 18:56:10 -0300

    FEAT: Conta Terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Conta!");
+        System.out.println("Número da Conta: ");
+        int conta = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Por favor, digite a Agência!");
+        System.out.println("Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o nome do Cliente!");
+        System.out.println("Nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta!");
+        System.out.println("Saldo: ");
+        double saldo = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
